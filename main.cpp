ad7194c8993519f9b695f77510add4f178125cab
Stepan Orlov <majorsteve@mail.ru>
2014-11-21 11:22:00 +0300

    OO implementation, version 1.

--- main.cpp
+++ main.cpp
@@ -1,48 +1,73 @@
 #include <QTextStream>
+#include <QVector>
 QTextStream cout(stdout);
 
-const int N = 79;
-int state[N];
-
-void init()
+class CellAut
 {
-    for (int i=0; i<N; ++i)
-        state[i] = 0;
-    state[N/2] = 1;
-}
-
-void printState()
-{
-    for (int i=0; i<N; ++i)
-        cout << (state[i] ?   '*' :   ' ');
-    cout << endl;
-}
-
-void nextState()
-{
-    const int Rule = 18;
-    int next[N];
-    for (int i=1; i<N-1; ++i)
+public:
+    explicit CellAut(int rule = 18, int size = 79) :
+        m_rule(rule),
+        m_size(size),
+        m_state(size),
+        m_nextState(size)
     {
-        int index = (state[i-1] << 2) | (state[i] << 1) | state[i+1];
-        next[i] = Rule & (1 << index) ?   1 :   0;
+        init();
     }
-    for (int i=1; i<N-1; ++i)
-        state[i] = next[i];
-}
+    int rule() const
+    {
+        return m_rule;
+    }
+    int size() const
+    {
+        return m_size;
+    }
+    const int *state() const
+    {
+        return m_state.data();
+    }
+    void print() const
+    {
+        for (int i=0; i<m_size; ++i)
+            cout << (m_state[i] ?   '*' :   ' ');
+        cout << endl;
+    }
+    void init(int state = 1<<16)
+    {
+        m_state.fill(0);
+        int stateIndex = m_size/2 + 16;
+        while (state)
+        {
+            m_state[stateIndex--] = state & 1 ?   1 :   0;
+            state >>= 1;
+        }
+    }
+    void next()
+    {
+        for (int i=1; i<m_size-1; ++i)
+        {
+            int index = (m_state[i-1] << 2) | (m_state[i] << 1) | m_state[i+1];
+            m_nextState[i] = m_rule & (1 << index) ?   1 :   0;
+        }
+        qSwap(m_state, m_nextState);
+    }
+    void run(int genCount)
+    {
+        for (int gen=0; gen<genCount; ++gen) {
+            print();
+            next();
+        }
+    }
 
-void run()
-{
-    const int MaxSteps = 25;
-    for (int i=0; i<MaxSteps; ++i) {
-        printState();
-        nextState();
-    }
-}
+private:
+    int m_rule;
+    int m_size;
+    QVector<int> m_state;
+    QVector<int> m_nextState;
+};
 
 int main(int argc, char *argv[])
 {
-    init();
-    run();
+    CellAut ca;
+    ca.run(25);
     return 0;
 }
